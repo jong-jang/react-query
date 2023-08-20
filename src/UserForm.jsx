da81f1e4ef2807600024dc0ae3ba866e1bc2a106
jong-jang <jjh0299@gmail.com>
2023-08-20 11:42:08 +0900

    useMutate로 서버데이터 변경 커스텀 훅 생성 및 이벤트 함수 생성

---
+++ src/UserForm.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { useUserQuery, useUserMutation } from './hooks/useUser';
+
+function UserForm() {
+	const { data } = useUserQuery(1);
+	const [UserName, setUserName] = useState(data?.name);
+
+	// useUserMutation훅은 호출해서 userMutation이란 이름으로 객체 반환
+	const userMutation = useUserMutation();
+
+	// 이벤트 발생시 서버데이터 변경요청하는 핸들러 함수
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// userMutation 객체의 mutate함수로 쿼리값 전달 ['변경할 데이터', 변경할 document고유 아이디값]
+		userMutation.mutate([UserName, 1]);
+	};
+
+	return <div>UserForm</div>;
+}
+
+export default UserForm;
