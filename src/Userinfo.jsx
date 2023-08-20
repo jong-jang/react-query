d960b244eaf161f73030bee4cd69f6719201b351
jong-jang <jjh0299@gmail.com>
2023-08-20 12:07:29 +0900

    폼추가

--- src/Userinfo.jsx
+++ src/Userinfo.jsx
@@ -1,13 +1,15 @@
+import UserForm from './UserForm';
 import { useUserQuery } from './hooks/useUser';
 
 function UserInfo() {
-	const { data, isSuccess, isError } = useUserQuery(3);
+	const { data, isSuccess, isError } = useUserQuery(1);
 
 	return (
 		<div>
 			<h1>User Information</h1>
 			{isSuccess && <h2>{data.name}</h2>}
 			{isError && <p>Data fething에 실패했습니다.</p>}
+			<UserForm />
 		</div>
 	);
 }
